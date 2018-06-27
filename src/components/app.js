b01141ae1b4c3603204cfb1c027ba4bd4053b401
dino soifua <dinopearson801@gmail.com>
2018-06-27 13:44:10 -0600

    App Setup

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Birtday Countdown</h1>
+      </div>
+    );
+  }
+}
