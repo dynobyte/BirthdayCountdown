eb74a8bd53794a236b9032c753ec2426e6a5d1eb
dino soifua <dinopearson801@gmail.com>
2018-06-27 14:51:45 -0600

    fixed src component and added heroku

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Birtday Countdown</h1>
+        <h1>Birthday Countdown</h1>
       </div>
     );
   }
